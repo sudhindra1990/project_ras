b49dbf464fc289dd721391fdea3153279703ea7d
sudhindra <sudhindra.harihar@gmail.com>
2024-09-23 22:06:19 +0530

    Update hello.py
    trail sample for Snyk

--- hello.py
+++ hello.py
@@ -4,5 +4,17 @@ Created on Sat Jul 20 23:22:08 2024
 
 @author: z0035k1b
 """
+from flask import Flask, jsonify
+import requests
 
-print("hello world")
+app = Flask(__name__)
+
+@app.route('/data', methods=['GET'])
+def fetch_data():
+    # This is a dummy URL; replace with an actual endpoint for real usage
+    response = requests.get('https://api.example.com/data')
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
